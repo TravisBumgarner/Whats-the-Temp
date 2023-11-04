7cb90c1d12a7aa20d47c777ab687ee91a9798834
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-11-04 15:42:47 -0700

    progress

--- README.md
+++ README.md
@@ -24,3 +24,14 @@ Make temp readings from a Particle Boron available to a React frontend via an Ex
 1. `yarn` install dependencies
 2. `yarn start`
 
+
+# Firebase notes
+
+firebase login
+
+firebase init
+
+When you're ready, deploy your web app
+Put your static files (e.g., HTML, CSS, JS) in your app's deploy directory (the default is "public"). Then, run this command from your app's root directory:
+
+firebase deploy
---
+++ cloud-function/functions/src/index.ts
@@ -0,0 +1,26 @@
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+admin.initializeApp()
+
+const db = admin.firestore()
+
+export const writeTempToFirestore = functions.https.onRequest(async (req, res): Promise<void> => {
+  try {
+    if (req.method !== 'POST') {
+      res.status(400).send('Invalid request method. Use POST.')
+    }
+
+    if (!req.body) {
+      res.status(400).send('Request body is missing.')
+    }
+
+    const requestData = req.body
+
+    const docRef = db.collection('temps').doc()
+    await docRef.set(requestData)
+
+    res.status(200).send('Data written to Firestore successfully')
+  } catch (error) {
+    res.status(500).send('Error writing data to Firestore: ' + error)
+  }
+})
